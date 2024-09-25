5fb7d49c181da66ce5e7f33423254e528d4f553d
Kellen Watt <kellen@wattsoft.dev>
2024-09-24 23:19:04 -0500

    Add reference header for linked lists

--- c/linked-list/main/linked-list/linked-list.h
+++ c/linked-list/main/linked-list/linked-list.h
@@ -1,4 +1,21 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <stdlib.h>
+
 struct List {
     int data;
     struct List* next;
 };
+
+struct List* list_new(int value);
+void list_free(struct List*);
+size_t list_len(const struct List*);
+void list_set(struct List*, size_t i, int value);
+int list_get(const struct List*, size_t i);
+struct List* list_push_front(struct List*, int value);
+struct List* list_push_back(struct List*, int value);
+struct List* list_insert(struct List*, size_t i, int value);
+struct List* list_remove(struct List*, size_t i);
+
+#endif
