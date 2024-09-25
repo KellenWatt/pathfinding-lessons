b1fcc352a7227e4ca67b3b154303383dbad252bf
Kellen Watt <kellen@wattsoft.dev>
2024-09-25 00:19:50 -0500

    Add basic signatures for stack

---
+++ c/stack/main/stack/stack.h
@@ -0,0 +1,23 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdlib.h>
+
+struct Stack {
+    struct StackNode* top;
+    size_t size;
+}
+
+struct StackNode {
+    int data;
+    struct StackNode* prev;
+}
+
+struct Stack* stack_new();
+void stack_free(struct Stack*);
+void stack_push(struct Stack*, int);
+void stack_pop(struct Stack*);
+int stack_top(const struct Stack*);
+size_t stack_len(const struct Stack*);
+
+#endif
