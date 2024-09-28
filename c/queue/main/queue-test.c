14d62109e3f536456ab38a2d1080d457c97655d3
Kellen Watt <kellen@wattsoft.dev>
2024-09-28 18:16:31 -0500

    Create reference implementation and tests for queue

--- c/queue/main/queue-test.c
+++ c/queue/main/queue-test.c
@@ -1,57 +1,58 @@
-#include "stack.h"
+#include "queue.h"
 #include "unity.h"
 
 #include <stdlib.h>
 
-void test_new_stack_is_empty() {
-    struct Stack* s = stack_new();
-    TEST_ASSERT_EQUAL_UINT64(0, stack_len(s));
-    stack_free(s);
+void test_new_queue_is_empty() {
+    struct Queue* q = queue_new();
+    TEST_ASSERT_EQUAL_UINT64(0, queue_len(q));
+    queue_free(q);
 }
 
 void test_push_adds_element() {
-    struct Stack* s = stack_new();
-    stack_push(s, 2);
-    TEST_ASSERT_EQUAL_UINT64(1, stack_len(s));
-    stack_free(s);
+    struct Queue* q = queue_new();
+    queue_push(q, 10);
+    TEST_ASSERT_EQUAL_UINT64(1, queue_len(q));
+    queue_free(q);
 }
 
 void test_can_push_multiple_elements() {
-    struct Stack* s = stack_new();
-    stack_push(s, 1);
-    stack_push(s, 2);
-    TEST_ASSERT_EQUAL_UINT64(2, stack_len(s));
-    stack_free(s);
+    struct Queue* q = queue_new();
+    queue_push(q, 12);
+    queue_push(q, 3);
+    TEST_ASSERT_EQUAL_UINT64(2, queue_len(q));
+    queue_free(q);
 }
 
-void test_push_adds_to_top() {
-    struct Stack* s = stack_new();
-    stack_push(s, 2);
-    TEST_ASSERT_EQUAL_INT(2, stack_top(s));
-    stack_push(s, 5);
-    TEST_ASSERT_EQUAL_INT(5, stack_top(s));
-    TEST_ASSERT_EQUAL_UINT64(2, stack_len(s));
-    stack_free(s);
+void test_push_adds_to_back() {
+    struct Queue* q = queue_new();
+    queue_push(q, 1);
+    TEST_ASSERT_EQUAL_INT(1, queue_front(q));
+    queue_push(q, 2);
+    TEST_ASSERT_EQUAL_INT(1, queue_front(q));
+    TEST_ASSERT_EQUAL_UINT64(2, queue_len(q));
+    queue_free(q);
 }
 
 void test_pop_works_on_empty() {
-    struct Stack* s = stack_new();
-    TEST_ASSERT_EQUAL_UINT64(0, stack_len(s));
-    stack_pop(s);
-    TEST_ASSERT_EQUAL_UINT64(0, stack_len(s));
-    stack_free(s);
+    struct Queue* q = queue_new();
+    TEST_ASSERT_EQUAL_UINT64(0, queue_len(q));
+    queue_pop(q);
+    TEST_ASSERT_EQUAL_UINT64(0, queue_len(q));
+    queue_free(q);
 }
 
-void test_pop_removes_top_element() {
-    struct Stack* s = stack_new();
-    stack_push(s, 5);
-    stack_push(s, 10);
-    stack_push(s, 15);
-    TEST_ASSERT_EQUAL_INT(15, stack_top(s));
-    stack_pop(s);
-    TEST_ASSERT_EQUAL_INT(10, stack_top(s));
-    stack_pop(s);
-    TEST_ASSERT_EQUAL_INT(5, stack_top(s));
-
-    stack_free(s);
+void test_pop_removes_front_element() {
+    struct Queue* q = queue_new();
+    queue_push(q, 5);
+    queue_push(q, 10);
+    queue_push(q, 15);
+    TEST_ASSERT_EQUAL_INT(5, queue_front(q));
+    queue_pop(q);
+    TEST_ASSERT_EQUAL_INT(10, queue_front(q));
+    queue_pop(q);
+    TEST_ASSERT_EQUAL_INT(15, queue_front(q));
+    
+    queue_free(q);
 }
+
--- c/queue/main/queue/queue.h
+++ c/queue/main/queue/queue.h
@@ -3,13 +3,21 @@
 
 #include <stdlib.h>
 
-struct Queue;
+struct QueueNode {
+    int data;
+    struct QueueNode* next;
+};
+
+struct Queue {
+    struct QueueNode* head;
+    size_t size;
+};
 
 struct Queue* queue_new();
 void queue_free(struct Queue*);
 void queue_push(struct Queue*, int);
 void queue_pop(struct Queue*);
-int queue_top(const struct Queue*);
+int queue_front(const struct Queue*);
 size_t queue_len(const struct Queue*);
 
 #endif
